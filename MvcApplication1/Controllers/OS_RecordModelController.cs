1650b72e9b2e4482bbfb1cc15f76e7875fafc96c
duxunmi <zhgt910@163.com>
2017-04-07 16:34:06 +0800

    12313123123123

---
+++ MvcApplication1/Controllers/OS_RecordModelController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication1.Models;
+
+namespace MvcApplication1.Controllers
+{
+    public class OS_RecordModelController : Controller
+    {
+        private MvcApplication1Context db = new MvcApplication1Context();
+
+        //
+        // GET: /OS_RecordModel/
+
+        public ActionResult Index()
+        {
+            return View(db.OS_RecordModel.ToList());
+        }
+
+        //
+        // GET: /OS_RecordModel/Details/5
+
+        public ActionResult Details(string id = null)
+        {
+            OS_RecordModel os_recordmodel = db.OS_RecordModel.Find(id);
+            if (os_recordmodel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(os_recordmodel);
+        }
+
+        //
+        // GET: /OS_RecordModel/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /OS_RecordModel/Create
+
+        [HttpPost]
+        public ActionResult Create(OS_RecordModel os_recordmodel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.OS_RecordModel.Add(os_recordmodel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(os_recordmodel);
+        }
+
+        //
+        // GET: /OS_RecordModel/Edit/5
+
+        public ActionResult Edit(string id = null)
+        {
+            OS_RecordModel os_recordmodel = db.OS_RecordModel.Find(id);
+            if (os_recordmodel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(os_recordmodel);
+        }
+
+        //
+        // POST: /OS_RecordModel/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(OS_RecordModel os_recordmodel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(os_recordmodel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(os_recordmodel);
+        }
+
+        //
+        // GET: /OS_RecordModel/Delete/5
+
+        public ActionResult Delete(string id = null)
+        {
+            OS_RecordModel os_recordmodel = db.OS_RecordModel.Find(id);
+            if (os_recordmodel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(os_recordmodel);
+        }
+
+        //
+        // POST: /OS_RecordModel/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            OS_RecordModel os_recordmodel = db.OS_RecordModel.Find(id);
+            db.OS_RecordModel.Remove(os_recordmodel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MvcApplication1/Models/OS_RecordModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    [Serializable]
+    public class OS_RecordModel
+    {
+        public string id { get; set; }
+        public string queueno { get; set; }
+    }
+}
